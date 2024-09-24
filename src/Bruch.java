dc0e8636cb319e05c6a4cbf88fe222c9ef5dff55
Claudio Landerer <landerer.claudio@gmail.com>
2024-09-24 12:05:31 +0200

    Klasse Bruch hinzugefügt

---
+++ src/Bruch.java
@@ -0,0 +1,9 @@
+public class Bruch {
+    private int zaehler;
+    private int nenner;
+
+    public Bruch(int zaehler, int nenner) {
+        this.zaehler = zaehler;
+        this.nenner = nenner;
+    }
+}
