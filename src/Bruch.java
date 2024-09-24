c495162edabb24b97c36645cc731ddc08cb9ee3c
Claudio Landerer <landerer.claudio@gmail.com>
2024-09-24 12:22:26 +0200

    Kapselung für Bruch für Zähler und Nenner implementiert.

--- src/Bruch.java
+++ src/Bruch.java
@@ -6,4 +6,26 @@ public class Bruch {
         this.zaehler = zaehler;
         this.nenner = nenner;
     }
+
+    public void setZaehler(int zaehler)
+    {
+        if(zaehler>=0)
+        {
+            this.zaehler = zaehler;
+        }
+
+    }
+    public void setNenner(int nenner)
+    {
+        if(nenner > 0)
+        {
+            this.nenner = nenner;
+        }
+    }
+
+    public int getNenner()
+    {
+        return this.nenner;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        Bruch bruch = new Bruch(2,4);
+        bruch.setNenner(5);
+        bruch.setZaehler(6);
+        int nenner = bruch.getNenner();
+        System.out.println("Der Nenner: " +  nenner);
     }
 }
