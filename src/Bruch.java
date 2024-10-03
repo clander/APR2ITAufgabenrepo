2b7ee95caeb716fb6e2ee9959ebf4333cb82bba6
Claudio Landerer <landerer.claudio@gmail.com>
2024-10-03 11:07:56 +0200

    BruchEinfach umbenannt in Bruch und alte Bruch-Klasse gelöscht.

--- src/Bruch.java
+++ src/Bruch.java
@@ -1,32 +1,44 @@
 public class Bruch {
-    private int zaehler;
-    private int nenner;
+     int zaehler;
+     int nenner;
 
-    public Bruch(int zaehler, int nenner) {
-
-        this.zaehler = zaehler;
-        this.nenner = nenner;
+    public void ausgeben()
+    {
+        System.out.println("Bruch: " + this.zaehler + "/" + nenner);
     }
 
-    public void setZaehler(int zaehler)
+    void multiplizieren(Bruch b)
     {
-        if(zaehler>=0)
+        zaehler = zaehler * b.zaehler;
+        nenner = nenner + b.nenner;
+       // b.nenner = 2; //Was passiert hier?
+       // b.zaehler = 4;
+    }
+
+    void gekuerztAusgeben()
+    {
+        kuerzen();
+        ausgeben();
+    }
+    void kuerzen()
+    {
+        var m = Math.abs(zaehler);
+        var n = Math.abs(nenner);
+        var r = m % n;
+        while(r>0)
         {
-            this.zaehler = zaehler;
+            m = n;
+            n = r;
+            r = m % n;
         }
-
+        zaehler = zaehler / n;
+        nenner = nenner / n;
     }
-    public void setNenner(int nenner)
+
+    void erweitern(int zahl)
     {
-        if(nenner > 0)
-        {
-            this.nenner = nenner;
-        }
+        zaehler = zaehler * zahl;
+        //zaehler *= zahl;
+        nenner = nenner * zahl;
     }
-
-    public int getNenner()
-    {
-        return nenner;
-    }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,27 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("***** Bruch 1 ******");
-        BruchEinfach bruchEinfach = new BruchEinfach();
-        bruchEinfach.zaehler = 24;
-        bruchEinfach.nenner = 0;
-        bruchEinfach.ausgeben();
+        Bruch bruch = new Bruch();
+        bruch.zaehler = 24;
+        bruch.nenner = 0;
+        bruch.ausgeben();
 
         System.out.println("***** Bruch 2 ******");
-        BruchEinfach bruchEinfach2 = new BruchEinfach();
-        bruchEinfach2.zaehler = 4;
-        bruchEinfach2.nenner = 3;
-        bruchEinfach2.ausgeben();
-        bruchEinfach2.nenner = 22;
-        bruchEinfach2.ausgeben();
-        bruchEinfach2.kuerzen();
-        bruchEinfach2.ausgeben();
-        bruchEinfach2.erweitern(3);
-        bruchEinfach2.ausgeben();
-        bruchEinfach2.kuerzen();
-        bruchEinfach2.ausgeben();
+        Bruch bruch2 = new Bruch();
+        bruch2.zaehler = 4;
+        bruch2.nenner = 3;
+        bruch2.ausgeben();
+        bruch2.nenner = 22;
+        bruch2.ausgeben();
+        bruch2.kuerzen();
+        bruch2.ausgeben();
+        bruch2.erweitern(3);
+        bruch2.ausgeben();
+        bruch2.kuerzen();
+        bruch2.ausgeben();
 
         System.out.println("***** Bruch 3 ******");
-        var brucheinfach3 = new BruchEinfach();
+        var brucheinfach3 = new Bruch();
         brucheinfach3.zaehler = 10;
         brucheinfach3.nenner = 5;
         //brucheinfach3.ausgeben();
@@ -31,19 +31,19 @@ public class Main {
         brucheinfach3.gekuerztAusgeben();
 
         System.out.println("***** Bruch 4 - Multiplikation ******");
-        BruchEinfach bruchEinfach4 = new BruchEinfach();
-        bruchEinfach4.zaehler = 5;
-        bruchEinfach4.nenner = 6;
+        Bruch bruch4 = new Bruch();
+        bruch4.zaehler = 5;
+        bruch4.nenner = 6;
         System.out.println("Bruch 4 vor Multiplikation:");
-        bruchEinfach4.ausgeben();
-        BruchEinfach bruchEinfach5 = new BruchEinfach();
+        bruch4.ausgeben();
+        Bruch bruchEinfach5 = new Bruch();
         bruchEinfach5.zaehler = 3;
         bruchEinfach5.nenner = 4;
         System.out.println("Bruch 5 vor Multiplikation:");
         bruchEinfach5.ausgeben();
-        bruchEinfach5.multiplizieren(bruchEinfach4);
+        bruchEinfach5.multiplizieren(bruch4);
         System.out.println("Bruch 4 nach Multiplikation:");
-        bruchEinfach4.ausgeben();
+        bruch4.ausgeben();
         System.out.println("Bruch 5 nach Multiplikation:");
         bruchEinfach5.ausgeben();
     }
