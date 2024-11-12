4a4fe50016887e27b8bb3fc3e5265f57665169ae
Claudio Landerer <landerer.claudio@gmail.com>
2024-11-12 12:14:42 +0100

    Beispiele zur Stringverwendung

--- src/K6/BruchEqualsDemo/Main.java
+++ src/K6/BruchEqualsDemo/Main.java
@@ -5,8 +5,10 @@ public class Main {
         System.out.println("**** Bruch-Equals-Demo *** ");
         Bruch b1 = new Bruch(3,4);
         Bruch b2 = new Bruch(3,4);
+        Bruch b3 = new Bruch(4,5);
         System.out.println(b1.equals(b2));
         System.out.println(b2.equals(b1));
+        System.out.println(b1.equals(b3));
         System.out.println(b1==b2);
         System.out.println(b2==b1);
     }
---
+++ src/K7/Main.java
@@ -0,0 +1,30 @@
+package K7;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s1 = "a";
+        String s2 = "b";
+        String s3 = "c";
+        String s4 = "B";
+
+        System.out.println(s2.compareTo(s1));
+        System.out.println(s2.compareTo(s3));
+        System.out.println(s2.compareTo(s2));
+        System.out.println(s2.compareTo(s4));
+        System.out.println((int)s2.charAt(0));
+        System.out.println((int)s4.charAt(0));
+
+
+        System.out.println("Das ist ein String".charAt(4));
+        String s5 = "Das ist ein netter String";
+        System.out.println(s5.toLowerCase());
+        System.out.println(s5.toUpperCase());
+
+        int i1 = 23;
+        System.out.println(String.valueOf(i1)+5);
+        System.out.println(i1+5);
+        System.out.println(i1+""+5);
+
+    }
+}
